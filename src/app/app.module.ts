e893e808779276b0e007fe4680a50a10eecdaa68
Pablo Ferreira <pabloferreira@RCA179.localdomain>
2021-01-10 15:49:42 -0300

    Changed to work with full routes and added error component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,37 @@ import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list/course-list.component';
 import { StarComponent } from './stars/star/star.component';
 import { ReplacePipe } from './pipes/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseListComponent, StarComponent, ReplacePipe],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full',
+      },
+      {
+        path: 'courses',
+        component: CourseListComponent,
+      },
+      {
+        path: '**', component: Error404Component
+      }
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
