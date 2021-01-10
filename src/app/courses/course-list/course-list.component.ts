788e2abd6d022043b2b9cd6c91b9fa9f67b8c2a5
Pablo Ferreira <pabloferreira@RCA179.localdomain>
2021-01-10 14:38:12 -0300

    Course list created

---
+++ src/app/courses/course-list/course-list.component.ts
@@ -0,0 +1,21 @@
+import { CourseService } from './../course.service';
+import { Component, OnInit } from '@angular/core';
+
+import { Course } from './course';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
+  styleUrls: ['./course-list.component.css']
+})
+export class CourseListComponent implements OnInit {
+
+  courses: Course[] = [];
+
+  constructor(private courseService: CourseService) {}
+
+  ngOnInit(): void {
+    this.courses = this.courseService.getAll();
+  }
+
+}
